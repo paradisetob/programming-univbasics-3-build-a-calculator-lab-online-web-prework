34b00c1d43b42c6ea51850cf9651832065513283
tracey brimage <onelovenilynn@gmail.com>
2019-09-05 22:05:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,25 +3,31 @@ def addition(num1, num2)
   return 20+10
 end
 
-def subtraction(20,10)
-  =-10
-  re
-
-
+def subtraction(num1,num2)
+  difference=20-10
+  return 20-10
 end
 
 def division(num1, num2)
+  quotient=20/10
+  return 20/10
 
 end
 
 def multiplication(num1, num2)
+  product=20*10
+  return 10*10
 
 end
 
 def modulo(num1, num2)
+  quotient=20%9
+  return 20%9
 
 end
 
 def square_root(num)
+  square_root=10
+  return square_root=9
 
 end
